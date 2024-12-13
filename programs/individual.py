1214b8bfe7d9fcf1a9eab76279b04e0b70fdbd8e
Joaquim Perez <jivkim@gmail.com>
2024-12-13 16:30:56 +0100

    feat(individual): predefined colors

--- programs/individual.py
+++ programs/individual.py
@@ -9,7 +9,21 @@ def run():
         filter=lambda x: int(x)
     ).execute()
 
-    pixels.handler[index] = inquirer.text(
-        message=f"hex value:",
-        filter= lambda x: int(x, 16)
+    color = inquirer.text(
+        message="hex value or one of r,g,b,w,m,y,c:"
     ).execute()
+
+    mapping = {
+        'r': 'ff0000',
+        'g': '00ff00',
+        'b': '0000ff',
+        'w': 'ffffff',
+        'm': 'ff00ff',
+        'y': 'ffff00',
+        'c': '00ffff',
+    }
+
+    if color in mapping:
+        color = mapping[color]
+
+    pixels.handler[index] = int(color, 16)
