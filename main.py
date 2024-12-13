18459ab629536714f88ec59d071719b81f4f13f1
Joaquim Perez <jivkim@gmail.com>
2024-12-13 01:03:28 +0100

    refactor: list choices in programs package __init__ list method

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 from InquirerPy import inquirer
-from InquirerPy.base.control import Choice
 import programs
 
 try:
     while True:
-        choice = inquirer.select(
+        f = inquirer.select(
             message="Program:",
-            choices=[
-                Choice(value="fill", name="Fill"),
-                Choice(value="deinit", name="Deinit"),
-                Choice(value="setpixelnumber", name="Set Pixel Number"),
-                Choice(value="setbrightness", name="Set Brightness"),
-            ],
+            choices=programs.list()
         ).execute()
-        p = getattr(programs, choice)
-        p.run()
+        f()
 except KeyboardInterrupt:
     print("Bye")
