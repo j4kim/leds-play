957e33b687326115f30ad0a7e5df47475afcbc59
Joaquim Perez <jivkim@gmail.com>
2024-12-13 01:43:00 +0100

    feat(inidividual): use hex value instead of rgb

--- programs/individual.py
+++ programs/individual.py
@@ -5,23 +5,16 @@ from config import config
 def run():
     index = inquirer.number(
         message="Pixel index:",
-        max_allowed=config['n'],
+        max_allowed=config['n'] - 1,
         min_allowed=0,
         filter=lambda x: int(x)
     ).execute()
 
     pixels = neopixel.NeoPixel(**config)
 
-    color = [0, 0, 0]
-    for i in range(3):
-        name = ["red", "green", "blue"][i]
-        max = 9
-        color[i] = inquirer.number(
-            message=f"value for {name} (0-{max}):",
-            min_allowed=0,
-            max_allowed=max,
-            filter= lambda x: int(int(x) * (255/max))
-        ).execute()
+    pixels[index] = inquirer.text(
+        message=f"hex value:",
+        filter= lambda x: int(x, 16)
+    ).execute()
 
-    pixels[index] = color
     pixels.show()
