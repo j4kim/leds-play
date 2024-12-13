50f9c8863684e7f95ae7cf3cd33c69256228b184
Joaquim Perez <jivkim@gmail.com>
2024-12-13 11:59:16 +0100

    feat(menu): preselect last choice

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from InquirerPy import inquirer
 import programs
 
+f = programs.list[0]['value']
+
 try:
     while True:
         f = inquirer.select(
             message="Program:",
-            choices=programs.list
+            choices=programs.list,
+            default=lambda _ : f
         ).execute()
         f()
 except KeyboardInterrupt:
