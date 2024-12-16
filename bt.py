b04a214d2f2baa59c528b956711d1ea6601cffaf
Joaquim Perez <jivkim@gmail.com>
2024-12-16 22:29:33 +0100

    example using asyncio.Queue()

--- bt.py
+++ bt.py
@@ -1,7 +1,27 @@
 from evdev import InputDevice, ecodes, categorize
+import asyncio
 
-gamepad = InputDevice('/dev/input/event2')
+event_queue = asyncio.Queue()
 
-for event in gamepad.read_loop():
-    if event.type == ecodes.EV_KEY:
-        print(categorize(event))
+async def listen_events():
+    gamepad = InputDevice('/dev/input/event2')
+    async for event in gamepad.async_read_loop():
+        if event.type == ecodes.EV_KEY:
+            await event_queue.put(categorize(event))
+
+async def log_events():
+    while True:
+        event = await event_queue.get()
+        print(event)
+        event_queue.task_done()
+
+async def main():
+    await asyncio.gather(
+        listen_events(),
+        log_events()
+    )
+
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    print("Programme arrêté.")
