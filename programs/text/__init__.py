5db9c358af1a7500634244fb1fee4bd98b2c9e64
Joaquim Perez <jivkim@gmail.com>
2024-12-22 00:28:06 +0100

    refactor(text): generate_bitmap returns data and textlength, handle padding in scroll method

--- programs/text/__init__.py
+++ programs/text/__init__.py
@@ -4,32 +4,38 @@ from pixels import pixels
 import time
 import os.path
 
-def generate_bitmap(text, padding = 6, size=8):
+def generate_bitmap(text, size=8):
     font = ImageFont.truetype(os.path.join(os.path.dirname(__file__), "PressStart2P-Regular.ttf"), size)
-    w = len(text) * size + padding * 2
+    w = len(text) * size
     image = Image.new("1", (w, size), 0)
     draw = ImageDraw.Draw(image)
-    draw.text((padding, 0), text, font=font, fill=1)
-    return np.array_split(list(image.getdata()), size)
+    draw.text((0, 0), text, font=font, fill=1)
+    return (
+        np.array_split(list(image.getdata()), size),
+        draw.textlength(text, font)
+    )
 
-def frame(values, offset = 0):
+
+def frame(bitmap, offset = 0):
     for y in range(7):
+        row = bitmap[y]
         for x in range(6):
-            color = pixels.default_color if values[y][x + offset] == 1 else 0
+            ox = x + offset
+            if ox < 0 or ox >= len(row):
+                color = 0
+            else:
+                color = pixels.default_color if row[ox] == 1 else 0
             pixels.set(x, y, color)
     pixels.show()
 
 def scroll(fps = 10):
-    values = generate_bitmap(input("Text: "))
-    offset = 0
-    try:
-        while True:
-            frame(values, offset)
-            time.sleep(1/fps)
-            offset += 1
-    except IndexError:
-        pass
+    bitmap, width = generate_bitmap(input("Text: "))
+    offset = -6
+    while offset < width:
+        frame(bitmap, offset)
+        time.sleep(1/fps)
+        offset += 1
 
 def char():
-    values = generate_bitmap(input("Char: "), 0)
-    frame(values)
+    bitmap, width = generate_bitmap(input("Char: "))
+    frame(bitmap)
