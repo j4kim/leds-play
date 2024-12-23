9f622bc2e343942d5de414007714343d84ee1888
Joaquim Perez <jivkim@gmail.com>
2024-12-24 00:41:43 +0100

    bt: bindings

--- bt/__init__.py
+++ bt/__init__.py
@@ -1,5 +1,22 @@
 import evdev
 
+bindings = {
+    '/dev/input/event2': {
+        46: 'arrow_up',
+        33: 'arrow_right',
+        32: 'arrow_down',
+        18: 'arrow_left',
+        35: 'x',
+        34: 'a',
+        36: 'b',
+        23: 'y',
+        37: 'left',
+        50: 'right',
+        49: 'select',
+        24: 'start',
+    }
+}
+
 def list_devices():
     devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
     for device in devices:
@@ -11,4 +28,4 @@ def test_device(path = '/dev/input/event2'):
     for event in gamepad.read_loop():
         if event.type == evdev.ecodes.EV_KEY:
             if (event.value == 1):
-                print(event.code)
+                print(bindings[path][event.code])
