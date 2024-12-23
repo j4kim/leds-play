9072977414b4b20368fb3176f8388026d63a5b7e
Joaquim Perez <jivkim@gmail.com>
2024-12-24 00:18:43 +0100

    move bt.py to bt/__init__.py with 2 functions list_devices and test_device

---
+++ bt/__init__.py
@@ -0,0 +1,13 @@
+import evdev
+
+def list_devices():
+    devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+    for device in devices:
+        print(device.path, device.name, device.phys)
+
+def test_device(path = '/dev/input/event2'):
+    gamepad = evdev.InputDevice(path)
+
+    for event in gamepad.read_loop():
+        if event.type == evdev.ecodes.EV_KEY:
+            print(evdev.categorize(event))
