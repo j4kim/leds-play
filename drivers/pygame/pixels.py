7a7ceadbc057c08e8722eb60fa4b0bb9865807a0
Joaquim Perez <jivkim@gmail.com>
2024-12-26 23:13:42 +0100

    feat(pygame driver): implement controller for pygame, transform_event to return same objects as in neopixel driver

--- drivers/pygame/pixels.py
+++ drivers/pygame/pixels.py
@@ -1,12 +1,25 @@
 import pygame
 import asyncio
 
+key_bindings = {
+    2: 'north',
+    0: 'east',
+    1: 'south',
+    3: 'west',
+    9: 'left',
+    10: 'right',
+    4: 'select',
+    6: 'start',
+}
+
 class Pixels:
     cells = ()
     default_color = 0xffffff
     screen = None
     scale = 40
     running = True
+    joysticks = []
+    on_event = None
 
     def __init__(self):
         pygame.init()
@@ -18,9 +31,46 @@ class Pixels:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
+                if self.on_event and self.joysticks and event.type in [pygame.JOYBUTTONUP, pygame.JOYBUTTONDOWN, pygame.JOYAXISMOTION]:
+                    self.on_event(self.transform_event(event))
             pygame.display.flip()
             await asyncio.sleep(1/60)
-            
+
+    def transform_event(self, event: pygame.event.Event):
+        binding = None
+        if event.type == pygame.JOYAXISMOTION:
+            v = round(event.value)
+            if event.axis == 1 and v == -1:
+                binding = 'arrow_up'
+            elif event.axis == 1 and v == 1:
+                binding = 'arrow_down'
+            elif event.axis == 0 and v == -1:
+                binding = 'arrow_left'
+            elif event.axis == 0 and v == 1:
+                binding = 'arrow_right'
+            value = abs(v)
+        else:
+            value = 1 if event.type == pygame.JOYBUTTONDOWN else 0
+            binding = key_bindings[event.button]
+        return {
+            'value': value,
+            'device': event.joy,
+            'binding': binding,
+        }
+
+    def listen_controllers(self, on_event):
+        count = pygame.joystick.get_count()
+        if count == 0:
+            raise Exception("No devices found")
+        for i in range(count):
+            joystick = pygame.joystick.Joystick(i)
+            self.joysticks.append(joystick)
+            self.on_event = on_event
+
+    def stop_listening_controllers(self):
+        self.joysticks = []
+        self.on_event = None
+
     def quit(self):
         self.running = False
 
