522ba261d627ad4df344683f152207cfa334b1e9
Joaquim Perez <jivkim@gmail.com>
2024-12-29 20:21:58 +0100

    ws_server.py sends messages to all connections using broadcast

--- web/ws_server.py
+++ web/ws_server.py
@@ -1,17 +1,21 @@
 import asyncio
-from websockets.asyncio.server import serve
+from websockets.asyncio.server import serve, broadcast
 
 
-async def echo(websocket):
+CONNECTIONS = set()
+
+async def register(websocket):
+    CONNECTIONS.add(websocket)
     async for message in websocket:
-        print(">", message)
-        await websocket.send(message)
-
+        broadcast(CONNECTIONS, message)
+    try:
+        await websocket.wait_closed()
+    finally:
+        CONNECTIONS.remove(websocket)
 
 async def main():
-    async with serve(echo, "localhost", 8765) as server:
+    async with serve(register, "localhost", 8765) as server:
         await server.serve_forever()
-
 
 if __name__ == "__main__":
     asyncio.run(main())
