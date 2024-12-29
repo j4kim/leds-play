84340827f300a8f1887c6417c60f14a02bf79d57
Joaquim Perez <jivkim@gmail.com>
2024-12-29 21:37:36 +0100

    refactor(ws_client): open connection once and use a queue to post messages to ws server

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import asyncio
 import prompt
 from driver import driver
+from web import ws_client
 
 async def main():
+    ws_task = asyncio.create_task(ws_client.connect_to_server())
     await asyncio.gather(
         prompt.run(),
         driver.run()
     )
+    ws_task.cancel()
 
 if __name__ == '__main__':
     asyncio.run(main())
--- web/ws_client.py
+++ web/ws_client.py
@@ -1,11 +1,26 @@
 import asyncio
 from websockets.asyncio.client import connect
+from InquirerPy.utils import patched_print
+
+queue = asyncio.Queue()
+
+connected = False
 
 
-async def send(message):
-    async with connect("ws://localhost:8765") as websocket:
-        await websocket.send(message)
+async def connect_to_server():
+    global connected
+    try:
+        async with connect("ws://localhost:8765") as websocket:
+            connected = True
+            while True:
+                message = await queue.get()
+                await websocket.send(message)
+    except Exception:
+        patched_print("Unable to connect to websocket server")
+    finally:
+        connected = False
 
 
 def play_sound(filename):
-    asyncio.create_task(send(f'{{"sound": "{filename}"}}'))
+    if connected:
+        queue.put_nowait(f'{{"sound": "{filename}"}}')
