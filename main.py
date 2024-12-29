4448180137410543575e3d13afde13fe44cdc81c
Joaquim Perez <jivkim@gmail.com>
2024-12-29 22:00:09 +0100

    start and stop websocket server from main.py

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import asyncio
 import prompt
 from driver import driver
 from web import ws_client
+from web import ws_server
 
 async def main():
-    ws_task = asyncio.create_task(ws_client.connect_to_server())
+    ws_server_task = asyncio.create_task(ws_server.start())
+    await ws_server.serving.wait()
+    ws_client_task = asyncio.create_task(ws_client.connect_to_server())
     await asyncio.gather(
         prompt.run(),
         driver.run()
     )
-    ws_task.cancel()
+    ws_client_task.cancel()
+    await ws_server.stop()
+    ws_server_task.cancel()
 
 if __name__ == '__main__':
     asyncio.run(main())
--- web/ws_server.py
+++ web/ws_server.py
@@ -1,6 +1,8 @@
 import asyncio
 from websockets.asyncio.server import serve, broadcast
+from InquirerPy.utils import patched_print
 
+serving = asyncio.Event()
 
 CONNECTIONS = set()
 
@@ -13,9 +15,12 @@ async def register(websocket):
     finally:
         CONNECTIONS.remove(websocket)
 
-async def main():
+async def start():
     async with serve(register, "localhost", 8765) as server:
+        patched_print("websocket server started on ws://localhost:8765")
+        serving.set()
         await server.serve_forever()
 
-if __name__ == "__main__":
-    asyncio.run(main())
+async def stop():
+    for connection in set(CONNECTIONS):
+        await connection.close()
