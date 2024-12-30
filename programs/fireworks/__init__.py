b3aee6e8d0c5f4ef2c1cea71dcc1622fa95c4c8a
Joaquim Perez <jivkim@gmail.com>
2024-12-30 01:04:57 +0100

    10 fireworks 🎇

--- programs/fireworks/__init__.py
+++ programs/fireworks/__init__.py
@@ -1,10 +1,12 @@
-from PIL import Image
 import os.path
-import numpy as np
-from driver import driver
-import asyncio
 from ..image import open_image, gif
+import asyncio
 
 async def run():
-    im = open_image("fireworks.gif", os.path.dirname(__file__))
-    await gif(im)
+    images = [open_image(f"{i}.gif", os.path.dirname(__file__)) for i in range(10)]
+
+    wait = [1, 0.5, 0.5, 2, 1, 0.2, 0.2, 0.2, 2, 0]
+
+    for i, image in enumerate(images):
+        await gif(image, 4)
+        await asyncio.sleep(wait[i])
