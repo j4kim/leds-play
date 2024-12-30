84923303ed542ae746e076d22cbb763ed6c9ef1f
Joaquim Perez <jivkim@gmail.com>
2024-12-30 18:45:19 +0100

    refactor(fireworks): fire method that plays sound and shows gif

--- programs/fireworks/__init__.py
+++ programs/fireworks/__init__.py
@@ -6,6 +6,10 @@ from InquirerPy import inquirer
 
 images = [open_image(f"gifs/{i}.gif", os.path.dirname(__file__)) for i in range(10)]
 
+async def fire(index):
+    ws_server.queue.put_nowait(f"firework-{index}")
+    await gif(images[index], 3)
+
 async def individual():
     index = 0
     while True:
@@ -18,12 +22,11 @@ async def individual():
         ).execute_async()
         if index == -1:
             return
-        await gif(images[index], 3)
+        await fire(index)
 
 async def run():
     wait = [1, 0.5, 0.5, 2, 1, 0.2, 0.2, 0.2, 2, 0]
 
-    for i, image in enumerate(images):
-        ws_server.queue.put_nowait(f"firework-{i}")
-        await gif(image, 3)
-        await asyncio.sleep(wait[i])
+    for i, s in enumerate(wait):
+        await fire(i)
+        await asyncio.sleep(s)
