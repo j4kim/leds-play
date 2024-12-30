c7af80767e235afaac7dd52e08b8da5332ff0ccf
Joaquim Perez <jivkim@gmail.com>
2024-12-30 01:10:01 +0100

    send firewoks sounds to websocket

--- programs/fireworks/__init__.py
+++ programs/fireworks/__init__.py
@@ -1,6 +1,7 @@
 import os.path
 from ..image import open_image, gif
 import asyncio
+from web import ws_server
 
 async def run():
     images = [open_image(f"{i}.gif", os.path.dirname(__file__)) for i in range(10)]
@@ -8,5 +9,6 @@ async def run():
     wait = [1, 0.5, 0.5, 2, 1, 0.2, 0.2, 0.2, 2, 0]
 
     for i, image in enumerate(images):
-        await gif(image, 4)
+        ws_server.queue.put_nowait(f"firework-{i}")
+        await gif(image, 3)
         await asyncio.sleep(wait[i])
