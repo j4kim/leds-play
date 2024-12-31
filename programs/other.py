ec4bf79b01b2a7e8f986a0bca04aaa460907a8a0
Joaquim Perez <jivkim@gmail.com>
2024-12-31 14:44:27 +0100

    refactor(prompt): move less used stuff in other submenu

---
+++ programs/other.py
@@ -0,0 +1,19 @@
+from programs import screen, zigzag, configure, individual, cursor, bt
+from driver import driver
+from tools import prompt_menu
+
+async def menu():
+    await prompt_menu([
+        {'value': driver.fill, 'name': 'Fill', 'only-for': 'neopixel'},
+        {'value': screen.draw, 'name': 'Draw screen'},
+        {'value': screen.fill, 'name': 'Fill screen'},
+        {'value': screen.animate, 'name': 'Animate random'},
+        {'value': zigzag.horizontal, 'name': 'H Zigzag'},
+        {'value': zigzag.vertical, 'name': 'V Zigzag', 'only-for': 'neopixel'},
+        {'value': configure.setBrighness, 'name': 'Set brightness'},
+        {'value': configure.setDefaultColor, 'name': 'Set default color'},
+        {'value': individual.run, 'name': 'Individual', 'only-for': 'neopixel'},
+        {'value': cursor.run, 'name': 'Cursor', 'only-for': 'neopixel'},
+        {'value': bt.testControllers, 'name': 'Test controllers'},
+        {'value': bt.freeThePixel, 'name': 'Free the pixel'},
+    ])
--- prompt.py
+++ prompt.py
@@ -1,4 +1,4 @@
-from programs import text, screen, zigzag, configure, individual, cursor, bt, quack, fireworks, happynewyear
+from programs import text, screen, quack, fireworks, happynewyear, other
 from driver import driver
 from tools import prompt_menu
 
@@ -6,23 +6,11 @@ async def run():
     await prompt_menu([
         {'value': happynewyear.run, 'name': 'Happy New Year'},
         {'value': happynewyear.schedule_for_midnight, 'name': 'Happy New Year (scheduled for midnight)'},
-        {'value': driver.fill, 'name': 'Fill', 'only-for': 'neopixel'},
+        {'value': quack.quack, 'name': 'Quack'},
         {'value': driver.clear, 'name': 'Clear'},
         {'value': text.menu, 'name': 'Text'},
-        {'value': quack.quack, 'name': 'Quack'},
-        {'value': fireworks.run, 'name': 'Fireworks'},
-        {'value': fireworks.individual, 'name': 'Individual firework'},
-        {'value': screen.draw, 'name': 'Draw screen'},
-        {'value': screen.fill, 'name': 'Fill screen'},
+        {'value': fireworks.individual, 'name': 'Fireworks'},
         {'value': screen.rand, 'name': 'Random screen'},
-        {'value': screen.animate, 'name': 'Animate random'},
-        {'value': zigzag.horizontal, 'name': 'H Zigzag'},
-        {'value': zigzag.vertical, 'name': 'V Zigzag', 'only-for': 'neopixel'},
-        {'value': configure.setBrighness, 'name': 'Set brightness'},
-        {'value': configure.setDefaultColor, 'name': 'Set default color'},
-        {'value': individual.run, 'name': 'Individual', 'only-for': 'neopixel'},
-        {'value': cursor.run, 'name': 'Cursor', 'only-for': 'neopixel'},
-        {'value': bt.testControllers, 'name': 'Test controllers'},
-        {'value': bt.freeThePixel, 'name': 'Free the pixel'},
+        {'value': other.menu, 'name': 'Other'},
     ])
     driver.quit()
