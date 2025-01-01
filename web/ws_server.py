9ce92ada4167ff64313bfa84ed6a948a42923165
Joaquim Perez <jivkim@gmail.com>
2025-01-01 16:07:22 +0100

    ws_server: a send function that encodes command to json

--- web/ws_server.py
+++ web/ws_server.py
@@ -1,6 +1,7 @@
 import asyncio
 from websockets.asyncio.server import serve, broadcast
 from InquirerPy.utils import patched_print
+import json
 
 queue = asyncio.Queue()
 
@@ -29,20 +30,23 @@ async def stop():
     for connection in set(CONNECTIONS):
         await connection.close()
 
+def send(command):
+    queue.put_nowait(json.dumps(command))
+
 def playsound(name):
-    queue.put_nowait(f'{{"action": "play_sound", "sound": "{name}"}}')
+    send({'action': 'play_sound', 'sound': name})
 
 def pausesound(name):
-    queue.put_nowait(f'{{"action": "pause_sound", "sound": "{name}"}}')
+    send({'action': 'pause_sound', 'sound': name})
 
 def resumesound(name):
-    queue.put_nowait(f'{{"action": "resume_sound", "sound": "{name}"}}')
+    send({'action': 'resume_sound', 'sound': name})
 
 def setvolume(name, volume):
-    queue.put_nowait(f'{{"action": "set_volume", "sound": "{name}", "volume": {volume}}}')
+    send({'action': 'set_volume', 'sound': name, 'volume': volume})
 
 def stopsounds():
-    queue.put_nowait(f'{{"action": "stop_sounds"}}')
+    send({'action': 'stop_sounds'})
 
 def fade(name, f, t, d):
-    queue.put_nowait(f'{{"action": "fade", "sound": "{name}", "from": {f}, "to": {t}, "duration": {d}}}')
+    send({'action': 'fade', 'sound': name, 'from': f, 'to': t, 'duration': d})
