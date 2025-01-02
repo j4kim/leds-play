eb1b48601475ab29e441442635cf1bb3b9b5a1ef
Joaquim Perez <jivkim@gmail.com>
2025-01-02 18:48:39 +0100

    refactor(snake): split Snake class into BaseGame and Snake. So BaseGame can be reused for any game. It handles the loop and the select button event to quit

---
+++ programs/games/base.py
@@ -0,0 +1,43 @@
+from driver import driver
+import asyncio
+from InquirerPy.utils import patched_print
+from abc import ABC, abstractmethod
+
+class BaseGame(ABC):
+    quit = asyncio.Event()
+    fps = 3
+
+    @classmethod
+    async def run(cls):
+        game = cls()
+        try:
+            driver.listen_controllers(game.handle_event)
+        except Exception as e:
+            print(e)
+            return
+        patched_print("Press select to quit")
+        await game.loop()
+        driver.clear()
+        driver.stop_listening_controllers()
+
+    async def loop(self):
+        while not self.quit.is_set():
+            self.frame()
+            await asyncio.sleep(1/self.fps)
+
+    @abstractmethod
+    def frame(self):
+        pass
+
+    def handle_event(self, event):
+        if event['value'] != 1:
+            return
+        if event['key'] == 'select':
+            self.quit.set()
+        key = event['key']
+        methodname = f'on_{key}'
+        method = getattr(self, methodname, None)
+        if method:
+            method()
+        else:
+            print(f"Method {methodname} not defined in {self.__class__.__name__}")
--- programs/games/snake.py
+++ programs/games/snake.py
@@ -1,47 +1,31 @@
 from driver import driver
-import asyncio
-from InquirerPy.utils import patched_print
+from .base import BaseGame
 
-class Snake:
+class Snake(BaseGame):
     dir = (0, -1)
     nextdir = (0, -1)
     head = (0, 7)
     body = [(0, 7)] * 10
-    quit = asyncio.Event()
-    fps = 3
 
-    @classmethod
-    async def run(cls):
-        snake = cls()
-        try:
-            driver.listen_controllers(snake.handle_event)
-        except Exception as e:
-            print(e)
-            return
-        patched_print("Press select to quit")
-        await snake.loop()
-        driver.clear()
-        driver.stop_listening_controllers()
+    def frame(self):
+        self.move()
+        self.draw()
 
-    async def loop(self):
-        while not self.quit.is_set():
-            self.move()
-            self.draw()
-            await asyncio.sleep(1/self.fps)
-
-    def handle_event(self, event):
-        if event['value'] != 1:
-            return
-        if event['key'] == 'arrow_up' and self.dir[0] != 0:
+    def on_arrow_up(self):
+        if self.dir[0] != 0:
             self.nextdir = (0, -1)
-        elif event['key'] == 'arrow_down' and self.dir[0] != 0:
+
+    def on_arrow_down(self):
+        if self.dir[0] != 0:
             self.nextdir = (0, 1)
-        elif event['key'] == 'arrow_left' and self.dir[1] != 0:
+
+    def on_arrow_left(self):
+        if self.dir[1] != 0:
             self.nextdir = (-1, 0)
-        elif event['key'] == 'arrow_right' and self.dir[1] != 0:
+
+    def on_arrow_right(self):
+        if self.dir[1] != 0:
             self.nextdir = (1, 0)
-        elif event['key'] == 'select':
-            self.quit.set()
 
     def inscreen(self, x, y):
         return 0 <= x <= 5 and 0 <= y <= 6
