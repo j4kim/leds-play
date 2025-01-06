cbedff6144263fdd1471a0b920e2b21e48ed2938
Joaquim Perez <jivkim@gmail.com>
2025-01-07 00:09:35 +0100

    refactor(paint.py): use a generator for black_white

--- programs/games/paint.py
+++ programs/games/paint.py
@@ -17,10 +17,10 @@ class Paint(BaseGame):
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
         )
-        self.blinker = self.get_blinker()
+        self.blinker = self.get_blinker_generator()
         self.color = (255, 255, 255)
         self.applied_at = 0
-        self.black_white_n = 0
+        self.black_white = self.get_black_white_generator()
 
     def frame(self):
         for y in range(7):
@@ -35,7 +35,7 @@ class Paint(BaseGame):
         self.x = (self.x + x) % 6
         self.y = (self.y + y) % 7
 
-    def get_blinker(self):
+    def get_blinker_generator(self):
         repeat = self.fps // 3
         while True:
             for color in [0x505050, self.color, 0x505050, None]:
@@ -45,17 +45,14 @@ class Paint(BaseGame):
     def apply(self):
         self.state[self.y][self.x] = self.color
 
-    def black_white(self):
-        self.black_white_n += 1
-        if self.black_white_n % 2 == 0:
-            self.color = (255, 255, 255)
-        else:
-            self.color = (0, 0, 0)
-
+    def get_black_white_generator(self):
+        while True:
+            yield (0, 0, 0)
+            yield (255, 255, 255)
 
     def on_arrow_up(self): self.move(0, -1)
     def on_arrow_down(self): self.move(0, 1)
     def on_arrow_left(self): self.move(-1, 0)
     def on_arrow_right(self): self.move(1, 0)
     def on_south(self): self.apply()
-    def on_east(self): self.black_white()
+    def on_east(self): self.color = next(self.black_white)
