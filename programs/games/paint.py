80fc3295d7166c01cd6a5e4a4bdfaac305bc64e5
Joaquim Perez <jivkim@gmail.com>
2025-01-06 23:18:52 +0100

    paint.py: a generator to make blink the cursor

--- programs/games/paint.py
+++ programs/games/paint.py
@@ -1,5 +1,6 @@
 from driver import driver
 from .base import BaseGame
+from InquirerPy.utils import patched_print
 
 class Paint(BaseGame):
     def __init__(self):
@@ -15,17 +16,25 @@ class Paint(BaseGame):
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
         )
+        self.blinker = self.get_blinker()
 
     def frame(self):
         for y in range(7):
             for x in range(6):
                 driver.set(x, y, self.state[y][x])
-        driver.set(self.x, self.y, 0xffffff)
+        driver.set(self.x, self.y, next(self.blinker))
         driver.show()
 
     def move(self, x, y):
         self.x = (self.x + x) % 6
         self.y = (self.y + y) % 7
+
+    def get_blinker(self):
+        while True:
+            for _ in range(self.fps//2):
+                yield 0x808080
+            for _ in range(self.fps//2):
+                yield 0
 
     def on_arrow_up(self): self.move(0, -1)
     def on_arrow_down(self): self.move(0, 1)
