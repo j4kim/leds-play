3aa2553b4be8aa47a601d9a33fb60e40341f1875
Joaquim Perez <jivkim@gmail.com>
2025-01-07 00:26:08 +0100

    feat(paint): color tuning using west, left and right

--- programs/games/paint.py
+++ programs/games/paint.py
@@ -19,6 +19,7 @@ class Paint(BaseGame):
         self.blinker = self.get_blinker_generator()
         self.color = (255, 255, 255)
         self.black_white = self.get_black_white_generator()
+        self.rgb_index = 0
 
     def frame(self):
         for y in range(7):
@@ -48,9 +49,23 @@ class Paint(BaseGame):
             yield (0, 0, 0)
             yield (255, 255, 255)
 
+    def change_rgb_index(self):
+        self.rgb_index = (self.rgb_index + 1) % 3
+
+    def tune(self, direction):
+        rgb = list(self.color)
+        curval = rgb[self.rgb_index]
+        newval = curval + direction * (255/10)
+        newval = int(min(255, max(0, newval)))
+        rgb[self.rgb_index] = newval
+        self.color = tuple(rgb)
+
     def on_arrow_up(self): self.move(0, -1)
     def on_arrow_down(self): self.move(0, 1)
     def on_arrow_left(self): self.move(-1, 0)
     def on_arrow_right(self): self.move(1, 0)
     def on_south(self): self.apply()
     def on_east(self): self.color = next(self.black_white)
+    def on_west(self): self.change_rgb_index()
+    def on_left(self): self.tune(-1)
+    def on_right(self): self.tune(1)
