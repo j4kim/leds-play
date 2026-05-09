c418454989d00c8988b7ccf471dde0d074259230
JoaquimPerez-tiffany <joaquim.perez@tiffany.com>
2026-05-10 01:50:33 +0200

    optimize(light): return if no change

--- programs/light/__init__.py
+++ programs/light/__init__.py
@@ -21,10 +21,14 @@ class Light:
             await asyncio.sleep(1 / 2)
 
     def frame(self):
-        driver.clear(False)
         motion = motion_driver.read()
-        self.lines += 1 if motion else -1
-        self.lines = max(0, min(self.lines, 7))
+        delta = 1 if motion else -1
+        lines = max(0, min(self.lines + delta, 7))
+        if self.lines == lines:
+            return
+        else:
+            self.lines = lines
+        driver.clear(False)
         for y in range(6, 6 - self.lines, -1):
             for x in range(6):
                 driver.set(x, y, self.color)
