3708fd190fb995fa81726387653756251b8b0f6e
JoaquimPerez-tiffany <joaquim.perez@tiffany.com>
2026-05-10 19:07:16 +0200

    file motion driver: check if file starts with 1

--- drivers/motion_drivers/file.py
+++ drivers/motion_drivers/file.py
@@ -9,7 +9,7 @@ class FileMotionDriver:
 
     def read(self):
         with open(self.file_path, "r") as f:
-            return f.read() == "1"
+            return f.read().startswith("1")
 
     def quit(self):
         pass
