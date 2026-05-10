cadbaba64741903c9c7d26162fea6c887db17d52
JoaquimPerez-tiffany <joaquim.perez@tiffany.com>
2026-05-10 23:41:28 +0200

    wip: Sun class

--- programs/light/sun/__init__.py
+++ programs/light/sun/__init__.py
@@ -1,12 +1,28 @@
 from . import config
+import asyncio
+from InquirerPy.utils import patched_print
+import json
+import os.path
+import time
+from datetime import datetime
 
 
 class Sun:
+    data_path = os.path.join(os.path.dirname(__file__), "data.json")
+    data = {}
+    last_update = datetime.fromtimestamp(0)
+
     def __init__(self):
-        pass
+        self.readJsonData()
+
+    def readJsonData(self):
+        mtime = os.path.getmtime(self.data_path)
+        self.last_update = datetime.fromtimestamp(mtime)
+        with open(self.data_path, "r") as f:
+            self.data = json.loads(f.read())
 
     async def request(self):
         pass
 
-    def isNight(self):
+    async def isNight(self):
         return True
